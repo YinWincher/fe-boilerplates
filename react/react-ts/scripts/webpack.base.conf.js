3c8205936b83d5cc87729977c0c0741d6d94bbdb
yinyongchuan <yinyongchuan@xiaomi.com>
2019-10-31 20:54:33 +0800

    record

---
+++ react/react-ts/scripts/webpack.base.conf.js
@@ -0,0 +1,98 @@
+
+
+module.exports = {
+    entry: {
+        app: './src/main.js'
+    },
+    output: {
+        path: config.build.assetsRoot,
+        publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+        filename: '[name].js',
+        chunkFilename: '[name].js'
+    },
+    resolve: {
+        modules: [path.join(__dirname, '../node_modules')],
+        extensions: ['.js', '.vue', '.less', '.css', '.scss', '.less'],
+        // fallback: [path.join(__dirname, '../node_modules')],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+            'vue-router$': 'vue-router/dist/vue-router.common.js',
+            'src': path.resolve(__dirname, '../src'),
+            'common': path.resolve(__dirname, '../src/common')
+        },
+        symlinks: false
+    },
+    externals:[{
+        xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
+    }],
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader'
+        },
+        {
+            test: /\.js$/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true
+                }
+            }],
+            include: /(src|node_modules\/noahv?)/
+        },
+        {
+            test: /\.css$/,
+            use: [
+                useCssExtract ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: useCssSourceMap
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.less$/,
+            use: [
+                useCssExtract ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: useCssSourceMap
+                    }
+                },
+                {
+                    loader: 'less-loader',
+                    options: {
+                        sourceMap: useCssSourceMap,
+                        javascriptEnabled: true
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: assetsPath('img/[name].[ext]')
+                }
+                
+            }]
+        }, {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: assetsPath('fonts/[name].[hash:7].[ext]')
+                }
+            }]
+        }]
+    },
+    plugins: [
+        // make sure to include the plugin!
+    ]
+}
